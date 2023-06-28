f741d31528c422dabb8ebb789efa27f38d107c02
Chris Clark <xchrisck@gmail.com>
2023-06-28 17:53:03 -0500

    updated the Nav bar to show different links if the user was on the about page or not with different links on that page based on if the user is logged in or not. About page is viewable to all users whether or not they're logged in

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 import { useSelector } from 'react-redux';
+import { useLocation } from 'react-router-dom';
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  const location = useLocation();
 
   return (
     <div className="nav">
@@ -13,25 +15,52 @@ function Nav() {
         <h2 className="nav-title">Prime Solo Project</h2>
       </Link>
       <div>
-        {/* If no user is logged in, show these links */}
-        {/* {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )} */}
+        {/* If the user is on the about page, show these links */}
+        {location.pathname==='/about' ? (
+          //on the about page but NOT logged in
+          !user.id?(
+            <>
+            <Link className="navLink" to="/home">
+              Login / Register
+            </Link>
+            </>
+       
+          ):(
+           //on the about page but IS logged in
+              <>
+                <Link className="navLink" to="/fridge">
+                  Back to Fridge
+                </Link>
+                <LogOutButton className="navLink" />
+                
+              </>
+            
+          )):(// else user is NOT on the about page, show these links
+            <>
+              {user.id?(
+                // user is not on about page but logged in
+                
+                <LogOutButton className="navLink" />
+                  ):(
+                    
+                <Link className="navLink" to="/home">
+                  Login / Register
+                </Link>
+                )}
+              
+              <Link className="navLink" to="/about">
+                About
+              </Link>
+              </>
+              
+            
 
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
+              
+            )
+        }
+        
 
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
+        
       </div>
     </div>
   );
