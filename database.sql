418caa19fe0c8bcd57955b6a121bd24f5de8b4c2
Chris Clark <xchrisck@gmail.com>
2023-07-10 14:06:54 -0500

    updated the readMe

--- database.sql
+++ database.sql
@@ -1,10 +1,19 @@
+--DB name needs to be 'pantry_pal'
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
+);
+CREATE TABLE "fridge" (
+	"id" SERIAL PRIMARY KEY,
+	"ingredient_name" VARCHAR NOT NULL,
+	"food_group_id" INT NOT NULL REFERENCES "food_group",
+	"amount" VARCHAR,
+	"expiration" DATE,
+	"user_id" INT REFERENCES "user"
+);
+CREATE TABLE "food_group" (
+	"id" SERIAL PRIMARY KEY,
+	"group_name" VARCHAR
 );
